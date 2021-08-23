a8e4e0a6fd6d31786cee6feebe6a53955fa09302
721HEMN <89218186+721HEMN@users.noreply.github.com>
2021-08-23 21:14:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (message.content === 'wara') {
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
-message.member.voiceChannel.join() 
+message.member.voiceChannel.join(877603234263482388) 
 
 .then(connection => { 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/6UVtqFzPgB `) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
+  client.channels.get("877603220753612811").send(
 `> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Rose 💙`) 
+client.user.setGame(`MAUHAMAD DARKPLAN`) 
 });
 
 
 
 
-client.login("");// Your Token
+client.login("ODc4MDA4MzA0MDA0MTI4Nzk4.YR669A.JioRaHY1X8z4tYX3cncROmRyHIw");// Your Token
